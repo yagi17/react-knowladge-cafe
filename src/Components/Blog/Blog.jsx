7741ecd58870a1987eb1f51b484915549c712a39
yagi17 <nafissadik785@gmail.com>
2024-03-12 23:24:44 +0600

    1st commit

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+    console.log(blog);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.PropTypes={
+    blog:PropTypes.object.isRequired
+}
+export default Blog;
